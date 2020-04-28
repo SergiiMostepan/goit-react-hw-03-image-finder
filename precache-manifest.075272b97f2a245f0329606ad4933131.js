c207221824472ab43a17cbb2e061a10a7774f8fc
Vika Mostepan <viktoriamostepan@gmail.com>
2020-04-28 22:31:21 +0300

    Updates

---
+++ precache-manifest.075272b97f2a245f0329606ad4933131.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f15bc6e0aba873ea686fafb57ef5917",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "cae46830fa984bd110ed",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f756ba8abf1e80aac6ed",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.456142a5.chunk.css"
+  },
+  {
+    "revision": "cae46830fa984bd110ed",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.baafe2b1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.baafe2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f756ba8abf1e80aac6ed",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.fe5479df.chunk.js"
+  },
+  {
+    "revision": "4992c48e99877600891f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ad90512f.js"
+  }
+]);
